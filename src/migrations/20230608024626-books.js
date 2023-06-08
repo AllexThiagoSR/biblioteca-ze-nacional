04bb25bb409193362bcfc9325026cddc02b69e9b
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-08 00:16:58 -0300

    feat: Implementa o migration da tabela books

--- src/migrations/20230608024626-books.js
+++ src/migrations/20230608024626-books.js
@@ -2,20 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'books',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        isRented: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+          field: 'is_rented',
+        },
+      },
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+     await queryInterface.dropTable('books');
   }
 };
