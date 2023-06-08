a25ca825981727a1f20609124556e7a5965214c0
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-08 00:34:05 -0300

    feat: Implementa a migration de rantals

--- src/migrations/20230608024704-rentals.js
+++ src/migrations/20230608024704-rentals.js
@@ -2,20 +2,50 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'rentals',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        userId: {
+          field: 'user_id',
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          references: {
+            model: 'users',
+            key: 'id'
+          },
+        },
+        bookId: {
+          field: 'book_id',
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          references: {
+            model: 'books',
+            key: 'id',
+          },
+        },
+        rentalDate: {
+          type: Sequelize.DATE,
+          field: 'rental_date',
+          defaultValue: null,
+        },
+        returnedAt: {
+          field: 'returned_at',
+          type: Sequelize.DATE,
+          defaultValue: null,
+        }
+      },
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('rentals');
   }
 };
