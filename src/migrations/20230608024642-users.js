78529fb2e38bb175acf7e695aec5e076f0104d1b
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-06-08 00:24:42 -0300

    feat: Implementa a migration da tabela users

--- src/migrations/20230608024642-users.js
+++ src/migrations/20230608024642-users.js
@@ -2,20 +2,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        role: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 2,
+        }
+      }
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
   }
 };
